33e8e3aeb2eb1499ed4d3bc860990ce50f99898a
EmboarXD <31370513+EmboarXD@users.noreply.github.com>
2020-09-29 11:02:20 -0400

    Update Question1.java

--- Question1.java
+++ Question1.java
@@ -1,9 +1,10 @@
+
 /* Activity 1.1 - Question 1 */
 
 class Question1 {
     public static void main(String[] args) {
 
-        /* Write your code here */
+        System.out.println("LeBron James");
 
     }
 }
